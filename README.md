93a08cbe1a2fe190e17f3d337da53c666e3aec96
FPlohl <florijan.plohl@gmail.com>
2022-03-22 15:44:54 +0100

    Added new files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Uart example
+
+This is tweaked uart example from NRF SDK.
---
+++ pca10056/blank/armgcc/_build/nrf52840_xxaa/app_error_handler_gcc.c.d
@@ -0,0 +1,155 @@
+_build/nrf52840_xxaa/app_error_handler_gcc.c.o: \
+ ../../../../../../components/libraries/util/app_error_handler_gcc.c \
+ ../../../../../../components/libraries/util/sdk_common.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/stdint.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/machine/_default_types.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/features.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/_newlib_version.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/_intsup.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/_stdint.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/string.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/_ansi.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/newlib.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/config.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/machine/ieeefp.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/reent.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/_ansi.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/_types.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/machine/_types.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/lock.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/cdefs.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/string.h \
+ ../config/sdk_config.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_os.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_macros.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/stdio.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/types.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/machine/types.h \
+ /home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/stdio.h \
+ ../../../../../../components/libraries/util/app_error_weak.h
+
+../../../../../../components/libraries/util/sdk_common.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/stdint.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/machine/_default_types.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/features.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/_newlib_version.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/_intsup.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/_stdint.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/string.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/_ansi.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/newlib.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/config.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/reent.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/_ansi.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/_types.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/machine/_types.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/lock.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/cdefs.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/string.h:
+
+../config/sdk_config.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/sdk_os.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52840.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_version.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/mpu_armv7.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52840.h:
+
+../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/sdk_macros.h:
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/stdio.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/types.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/machine/types.h:
+
+/home/florijan/gnuarmemb/gcc-arm-none-eabi-9-2019-q4-major/arm-none-eabi/include/sys/stdio.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
